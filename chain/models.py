66614c4ce4bb21a869fc9e4b48171d2053e7c4fa
Crazilina <19alina91@bk.ru>
2024-09-02 19:08:16 +0200

    Реализована модель ElectronicsChain с иерархической структурой для заводов, розничных сетей и индивидуальных предпринимателей. Добавлена модель Product и связанные с ней поля.

---
+++ chain/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.conf import settings
+
+
+class Product(models.Model):
+    """
+    Модель, представляющая продукт, который производится и продается в торговой сети.
+    """
+
+    # Название продукта
+    name = models.CharField(max_length=255, verbose_name="Название продукта")
+
+    # Модель продукта
+    model = models.CharField(max_length=100, verbose_name="Модель продукта")
+
+    # Дата выхода продукта на рынок
+    release_date = models.DateField(verbose_name="Дата выхода на рынок")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return f"{self.name} ({self.model})"
+
+
+class ElectronicsChain(models.Model):
+    """
+    Модель, представляющая звено в торговой сети по продаже электроники.
+    Включает в себя заводы, розничные сети и индивидуальных предпринимателей.
+    """
+
+    FACTORY = 'factory'
+    RETAIL = 'retail'
+    ENTREPRENEUR = 'entrepreneur'
+    NODE_TYPE_CHOICES = [
+        (FACTORY, 'Завод'),
+        (RETAIL, 'Розничная сеть'),
+        (ENTREPRENEUR, 'Индивидуальный предприниматель'),
+    ]
+
+    # Название звена торговой сети
+    name = models.CharField(max_length=255, verbose_name="Название звена")
+
+    # Тип звена в торговой сети (завод, розничная сеть, индивидуальный предприниматель)
+    node_type = models.CharField(max_length=20, choices=NODE_TYPE_CHOICES, verbose_name="Тип звена")
+
+    # Ссылка на поставщика, который находится выше в иерархии
+    supplier = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='clients',
+                                 verbose_name="Поставщик")
+
+    # Контактный email для связи с представителями звена
+    email = models.EmailField(verbose_name="Email для связи")
+
+    # Страна, в которой находится звено сети
+    country = models.CharField(max_length=100, verbose_name="Страна расположения")
+
+    # Город, в котором находится звено сети
+    city = models.CharField(max_length=100, verbose_name="Город расположения")
+
+    # Улица, на которой находится звено сети
+    street = models.CharField(max_length=100, verbose_name="Улица расположения")
+
+    # Номер дома, где расположено звено сети
+    house_number = models.CharField(max_length=20, verbose_name="Номер дома")
+
+    # Продукты, которые связаны с этим звеном сети
+    products = models.ManyToManyField(Product, related_name='electronics_chains', verbose_name="Продукты")
+
+    # Задолженность перед поставщиком в денежном выражении с точностью до копеек
+    debt = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Задолженность перед поставщиком")
+
+    # Дата и время создания записи в базе данных
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+
+    # Пользователь, ответственный за это звено сети
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Владелец")
+
+    class Meta:
+        verbose_name = "Звено сети электроники"
+        verbose_name_plural = "Звенья сети электроники"
+
+    def __str__(self):
+        return f"{self.name} ({self.get_node_type_display()})"
